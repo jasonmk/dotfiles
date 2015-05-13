c973497b9d2e52c435fcaf795a31b7b451ffcada
Jason M. Kusar <jason@kusar.net>
2015-05-13 15:05:02 -0400

    Better PS1

--- ps1.sh
+++ ps1.sh
@@ -1,36 +1,30 @@
-###  Bash shell prompt with RVM and Git info  ###
-###  REQUIRES git-prompt.sh provided with Git ###
-
-# $ cp git-prompt.sh ~/.git-prompt.sh
-# $ echo source  ~/.git-completion.sh >> ~/.bashrc
-
-# in ~/.bashrc
- BLACK="\e[30m"
-   RED="\e[31m"
- GREEN="\e[32m"
-YELLOW="\e[33m"
-  BLUE="\e[34m"
-PURPLE="\e[35m"
-  CYAN="\e[36m"
- WHITE="\e[37m"
-
-COLOR=$WHITE #default
-
-function git_dirty {
-  git status -s  2>/dev/null | sed /^#/d | wc -l | tr -d ' '
+function git-branch-name {
+  echo $(git symbolic-ref HEAD 2>/dev/null | awk -F/ {'print $NF'})
 }
 
-function git_repo {
-  git rev-parse --show-toplevel
+function git-dirty {
+  if ! git status -uno 2>/dev/null | grep -q "nothing to commit"
+  then
+    echo "*"
+  fi
 }
 
-export GIT_PS1_SHOWDIRTYSTATE=true
-export GIT_PS1_SHOWUNTRACKEDFILES=true
-PS1="\\n\\n\
-$PURPLE\$(git_dirty) \
-$YELLOW\$(__git_ps1 \"#%s\")\
-$COLOR \
-\$(git_repo) \
-\$(rvm-prompt)\
-\\n\
-[\w] "
+function git-unpushed {
+  brinfo=$(git branch -v | grep $(git-branch-name))
+  if [[ $brinfo =~ ("[ahead "([[:digit:]]*)]) ]]
+  then
+    echo "(${BASH_REMATCH[2]})"
+  fi
+}
+
+function git-untracked {
+  git ls-files --others --exclude-standard --error-unmatch -- '*' > /dev/null 2>&1
+  if [ $? -eq 0 ]
+  then
+    echo "%"
+  fi
+}
+
+PS1='\[\e[1;37m\]$(rvm-prompt)\n\[\e[0;36m\]\T \d \[\e[1;30m\][\[\e[1;33m\]\u\[\e[1;34m\]@\h\[\e[1;30m\]\[\e[0;32m\]$(git branch &>/dev/null; if [ $? -eq 0 ]; then echo " ($(git-branch-name) $(git-dirty)$(git-untracked)$(git-unpushed))"; fi)\[\e[1;30m\]] \[\e[1;37m\]\w\[\e[0;37m\] \n($SHLVL:\!)\$ '
+
+export PS1
